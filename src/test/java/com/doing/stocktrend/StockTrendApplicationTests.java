0eb7b4e0520a70552121cf5b44d5481b53699e4f
954657174@qq.com <czyxx123>
2020-04-29 01:17:50 +0800

    爬听书网里面的小说

---
+++ src/test/java/com/doing/stocktrend/StockTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doing.stocktrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
